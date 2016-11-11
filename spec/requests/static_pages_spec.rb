867e0ead98467e98058ec4530df9c475f419bfaa
ccsfunk <funkccs@gmail.com>
2016-11-11 14:29:59 +0100

    modificado static pages y se a agregado un new file

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,6 +12,8 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -19,13 +21,15 @@ describe "Static pages" do
     end
     it "should have the title 'Home'" do 
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+#      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+       expect(page).to have_title("#{base_title} | Home")
     end 
   end 
   describe "Help page" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_content('Help')
+#      expect(page).to have_content('Help')
+       expect(page).to have_title("#{base_title} | Help")
     end
     it "should have the title 'help'" do
       visit '/static_pages/help'
@@ -35,13 +39,26 @@ describe "Static pages" do
   describe "About page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_content('About Us')
+#      expect(page).to have_content('About Us')
+       expect(page).to have_title("#{base_title} | About Us")
     end
-    it "should have the title 'about Us'" do
+    it "should have the title 'about'" do
       visit '/static_pages/about'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+  describe "Contact page" do
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Sample App')
+    end
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+#      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+       expect(page).to have_title("#{base_title} | Contact")
+    end
+  end
+
 end 
 
 
