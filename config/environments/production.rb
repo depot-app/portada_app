7942d544727f5756880506f1cac975d2edfb5673
ccsfunk <funkccs@gmail.com>
2016-11-20 18:33:32 +0100

    Modifield production.rb heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -83,4 +84,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+#secret_key_base
+  secret_key_base = ENV["SECRET_KEY_BASE"]
 end
